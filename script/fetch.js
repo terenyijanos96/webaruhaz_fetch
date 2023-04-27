060d61fe4b7126fc772cf8b73e1e263c47843543
Terényi János <terenyi.janos96@gmail.com>
2023-04-27 21:20:10 +0200

    többi fetch függvény létrehozása (POST, DELETE)

--- script/fetch.js
+++ script/fetch.js
@@ -1,8 +1,39 @@
 export function adatBeolvas(vegpont, callback) {
-    fetch(vegpont)
-      .then((response) => response.json())
-      .then((data) => {
-        callback(data);
-      })
-      .catch((err) => console.log(err));
-  }
+  fetch(vegpont)
+    .then((response) => response.json())
+    .then((data) => {
+      callback(data);
+    })
+    .catch((err) => console.log(err));
+}
+
+export function adatFeltolt(vegpont, adat) {
+  fetch(vegpont, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(adat),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => console.log(error));
+}
+
+export function adatTorol(vegpont, id) {
+  vegpont = `${vegpont} / ${id}`;
+  console.log(vegpont);
+  fetch(vegpont, {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => console.log(error));
+}
